6c900023e8109316c1fab09ea36e08c4177935d0
Rackyrr <valentin.lestrelin@free.fr>
2022-12-14 20:06:56 +0100

    Boutton switch style sheet

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleTheme() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementsByTagName('link')[0];
+
+    // Change the value of href attribute 
+    // to change the css sheet.
+    if (theme.getAttribute('href') == 'style.css') {
+        theme.setAttribute('href', 'nether_style.css');
+    } else {
+        theme.setAttribute('href', 'style.css');
+    }
+}
