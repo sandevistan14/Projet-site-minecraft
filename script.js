19a9d57b8fd2a5b72dac81d999d279c08ea3a4e2
Robin <robin.lerouge@etu.univ-amu.fr>
2022-12-15 10:06:11 +0100

    meta + correction encrage + petit detail
    Co-authored-by: Rackyrr <Rackyrr@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+/******************************************************************************
+*********************    bouton switch style    *******************************
+******************************************************************************/
+
 function toggleTheme() {
     // Obtains an array of all <link>
     // elements.
     // Select your element using indexing.
     var theme = document.getElementsByTagName('link')[0];
-
-    // Change the value of href attribute 
+    // Change the value of href attribute
     // to change the css sheet.
     if (theme.getAttribute('href') == 'style.css') {
         theme.setAttribute('href', 'nether_style.css');
     } else {
         theme.setAttribute('href', 'style.css');
     }
+
+    // var image = document.getElementById('a')[7];
+    // if(image.getAttribute('src') == 'images/bouton_remontev2') {
+    //     image.setAttribute('src', 'images/bouton_remonte_nether.png');
+    // } else {
+    //     image.setAttribute('src', 'images/bouton_remontev2.png');
+    // }
+}
+
+function jump(h){
+    var top = document.getElementById(h).offsetTop;
+    window.scrollTo(0, top);
 }
