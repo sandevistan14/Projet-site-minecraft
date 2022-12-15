98730c168b9f0b89185144fe1b4b7cc40691a639
Robin <robin.lerouge@etu.univ-amu.fr>
2022-12-15 11:57:08 +0100

    finalisation du script + mise dans des dodsiers
    Co-authored-by: Rackyrr <Rackyrr@users.noreply.github.com>

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -7,20 +7,16 @@ function toggleTheme() {
     // elements.
     // Select your element using indexing.
     var theme = document.getElementsByTagName('link')[0];
+    var image = document.getElementById('ancrage_img');
     // Change the value of href attribute
     // to change the css sheet.
-    if (theme.getAttribute('href') == 'style.css') {
-        theme.setAttribute('href', 'nether_style.css');
+    if (theme.getAttribute('href') == '/asset/css/style.css') {
+        theme.setAttribute('href', '/asset/css/nether_style.css');
+        image.setAttribute('src','/asset/image/bouton_remonte_nether.png')
     } else {
-        theme.setAttribute('href', 'style.css');
+        theme.setAttribute('href', '/asset/css/style.css');
+        image.setAttribute('src', '/asset/image/bouton_remontev2.png')
     }
-
-    // var image = document.getElementById('a')[7];
-    // if(image.getAttribute('src') == 'images/bouton_remontev2') {
-    //     image.setAttribute('src', 'images/bouton_remonte_nether.png');
-    // } else {
-    //     image.setAttribute('src', 'images/bouton_remontev2.png');
-    // }
 }
 
 function jump(h){
