fdb3f2fd94b3bc0d26f39862b398730055e8dca4
Robin <robin.lerouge@etu.univ-amu.fr>
2022-12-17 20:32:20 +0100

    correction des liens des pages

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -10,12 +10,12 @@ function toggleTheme() {
     var image = document.getElementById('ancrage_img');
     // Change the value of href attribute
     // to change the css sheet.
-    if (theme.getAttribute('href') == '/asset/css/style.css') {
-        theme.setAttribute('href', '/asset/css/nether_style.css');
-        image.setAttribute('src','/asset/image/bouton_remonte_nether.png')
+    if (theme.getAttribute('href') == '../css/style.css') {
+        theme.setAttribute('href', '../css/nether_style.css');
+        image.setAttribute('src','../image/bouton_remonte_nether.png')
     } else {
-        theme.setAttribute('href', '/asset/css/style.css');
-        image.setAttribute('src', '/asset/image/bouton_remontev2.png')
+        theme.setAttribute('href', '../css/style.css');
+        image.setAttribute('src', '../image/bouton_remontev2.png')
     }
 }
 
