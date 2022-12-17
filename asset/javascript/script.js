40f0821d229404dee6f4830fef9cadc9caa6e935
Robin <robin.lerouge@etu.univ-amu.fr>
2022-12-17 21:11:40 +0100

    création d'une deuxime fonction changement de theme pour l'index.html

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -24,3 +24,19 @@ function jump(h){
     window.scrollTo(0, top);
 }
 
+function toggleTheme2() {
+    // Obtains an array of all <link>
+    // elements.
+    // Select your element using indexing.
+    var theme = document.getElementsByTagName('link')[0];
+    var image = document.getElementById('ancrage_img');
+    // Change the value of href attribute
+    // to change the css sheet.
+    if (theme.getAttribute('href') == 'asset/css/style.css') {
+        theme.setAttribute('href', 'asset/css/nether_style.css');
+        image.setAttribute('src','asset/image/bouton_remonte_nether.png')
+    } else {
+        theme.setAttribute('href', 'asset/css/style.css');
+        image.setAttribute('src', 'asset/image/bouton_remontev2.png')
+    }
+}
