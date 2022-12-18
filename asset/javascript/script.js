024f9fb1a84c7f4e7ca30945b574b8575fc4d095
Robin <robin.lerouge@etu.univ-amu.fr>
2022-12-18 18:06:53 +0100

    rajout text + correction fautes

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -12,14 +12,14 @@ function toggleTheme() {
     // to change the css sheet.
     if (theme.getAttribute('href') == '../css/style.css') {
         theme.setAttribute('href', '../css/nether_style.css');
-        image.setAttribute('src','../image/bouton_remonte_nether.png')
+        image.setAttribute('src', '../image/bouton_remonte_nether.png')
     } else {
         theme.setAttribute('href', '../css/style.css');
         image.setAttribute('src', '../image/bouton_remontev2.png')
     }
 }
 
-function jump(h){
+function jump(h) {
     var top = document.getElementById(h).offsetTop;
     window.scrollTo(0, top);
 }
@@ -34,7 +34,7 @@ function toggleTheme2() {
     // to change the css sheet.
     if (theme.getAttribute('href') == 'asset/css/style.css') {
         theme.setAttribute('href', 'asset/css/nether_style.css');
-        image.setAttribute('src','asset/image/bouton_remonte_nether.png')
+        image.setAttribute('src', 'asset/image/bouton_remonte_nether.png')
     } else {
         theme.setAttribute('href', 'asset/css/style.css');
         image.setAttribute('src', 'asset/image/bouton_remontev2.png')
